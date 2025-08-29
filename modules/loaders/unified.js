1305060df6c270b64e95735b77ee46bc6f95da73
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2025-08-29 22:26:41 +0800

    doc: Update images; Update descriptions on loader

--- modules/loaders/unified.js
+++ modules/loaders/unified.js
@@ -1,5 +1,5 @@
 const { dataPath, configApi } = globalThis.LiteLoader ?
-    await import("./liteloader.js") :
+    await import("./liteloader.js") : // TODO: LiteLoader doesn't support ESM imports - maybe drop support or use tools to auto transform?
     await import("./qwqnt.js");
 
 export {
